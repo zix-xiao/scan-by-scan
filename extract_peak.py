ed3119927b91d5ca91a5a332803f2240639bb030
zix-xiao <cory.zixuan.xiao@outlook.com>
2023-12-13 12:42:11 +0100

    add support to align peaks with MQ using RT start and end

--- extract_peak.py
+++ extract_peak.py
@@ -14,16 +14,11 @@ from result_analysis.result_analysis import SBSResult
 def extract_peaks_and_analyze(
     MQ_ref_path: str,
     MQ_exp_path: str,
-    activation_path: str = (
-        "/mnt/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/raw/msconvert/"
-        "1FDR_BBM_647_P241_02_07_ssDDA_MIA_001_ScanByScan_RTtol1.0_MZtolNone_peakRange_lasso_cd_abthres0.001_missabthres0.5_convergence_NoIntercept_pred/"
-        "1FDR_BBM_647_P241_02_07_ssDDA_MIA_001_ScanByScan_RTtol1.0_MZtolNone_peakRange_threshold_abthres0.001_missabthres0.5_convergence_NoIntercept_pred_output_activationMinima.npy"
-    ),
-    MS1ScansNoArray_path: str = (
-        "/mnt/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/raw/msconvert/"
-        "BBM_647_P241_02_07_ssDDA_MIA_001_MS1Scans_NoArray.csv"
-    ),
-    RT_ref: str = "predicted_RT",
+    activation_path: str = "/mnt/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/raw/msconvert/1FDR_BBM_647_P241_02_07_ssDDA_MIA_001_ScanByScan_RTtol1.0_MZtolNone_peakRange_lasso_cd_abthres0.001_missabthres0.5_convergence_NoIntercept_pred/1FDR_BBM_647_P241_02_07_ssDDA_MIA_001_ScanByScan_RTtol1.0_MZtolNone_peakRange_threshold_abthres0.001_missabthres0.5_convergence_NoIntercept_pred_output_activationMinima.npy",
+    MS1ScansNoArray_path: str = "/mnt/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/raw/msconvert/BBM_647_P241_02_07_ssDDA_MIA_001_MS1Scans_NoArray.csv",
+    ref_RT_apex: str = "predicted_RT",
+    ref_RT_start: None | str = "Retention time start",
+    ref_RT_end: None | str = "Retention time end",
     return_peak_result: bool = True,
     peak_width_thres=(3, None),
 ):
@@ -34,7 +29,7 @@ def extract_peaks_and_analyze(
     MS1Scans_NoArray = pd.read_csv(os.path.join(MS1ScansNoArray_path))
     activation = np.load(activation_path)
     find_peak_cond = "_".join(str(a) for a in peak_width_thres)
-    find_peak_cond += "_"+RT_ref.replace(' ', '_')
+    find_peak_cond += "_" + ref_RT_apex.replace(" ", "_")
 
     logging.basicConfig(
         filename=os.path.join(result_dir, find_peak_cond + ".log"),
@@ -47,7 +42,9 @@ def extract_peaks_and_analyze(
             Maxquant_result=Maxquant_result_dict,  # 100 rows for testing
             activation=activation,
             MS1ScansNoArray=MS1Scans_NoArray,
-            RT_ref=RT_ref,
+            ref_RT_apex=ref_RT_apex,
+            ref_RT_start=ref_RT_start,
+            ref_RT_end=ref_RT_end,
             return_peak_result=return_peak_result,
             peak_width_thres=peak_width_thres,
         )
@@ -64,7 +61,9 @@ def extract_peaks_and_analyze(
             Maxquant_result=Maxquant_result_dict,
             activation=activation,
             MS1ScansNoArray=MS1Scans_NoArray,
-            RT_ref=RT_ref,
+            ref_RT_apex=ref_RT_apex,
+            ref_RT_start=ref_RT_start,
+            ref_RT_end=ref_RT_end,
             return_peak_result=return_peak_result,
             peak_width_thres=peak_width_thres,
         )
