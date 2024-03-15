a5d4f43581f4d68720ce4ceb621e8ef1c03f19af
zix-xiao <cory.zixuan.xiao@outlook.com>
2024-03-15 14:37:41 +0100

    update --mem-per-cpu add unit Gb

--- scripts/ScanByScan_slurm.sh
+++ scripts/ScanByScan_slurm.sh
@@ -4,18 +4,12 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --output=/cmnfs/proj/ORIGINS/data/protMSD/slurm_out/%x_%j.out
 #SBATCH --job-name=SBS
-#SBATCH --mem-per-cpu=50
+#SBATCH --mem-per-cpu=16Gb
 #SBATCH --mail-user=zixuan.xiao@tum.de
 
 
-#mzML='/cmnfs/proj/ORIGINS/data/protMSD/GCF_profile/msconvert_profile.mzML'
 config_path='/cmnfs/proj/ORIGINS/protMSD/maxquant/ScanByScan/utils/sbs_config.json'
-# mzML='/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/raw/msconvert/BBM_647_P241_02_07_ssDDA_MIA_001.mzML'
-# maxquant='/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/MQ/combined/txt/evidence_1_1_FilteredByClosestRT_transfer_RT_pred_filtered_withIso_expRTrange.pkl'
-# maxquant_exp='/cmnfs/proj/ORIGINS/data/ecoli/ss/DDA/MQ/combined/txt/evidence_1.txt'
-#maxquant='/cmnfs/proj/ORIGINS/data/protMSD/GCF_profile/combined/txt/evidence_transfer_RT_pred_filtered_withIso.pkl'
 
-## printing shell variables is complicated by escaping
 source $HOME/condaInit.sh
 conda activate sbs
 python /cmnfs/proj/ORIGINS/protMSD/maxquant/ScanByScan/sbs_runner.py \
